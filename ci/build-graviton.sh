5485a5c300b061746a7c5aa07ce0263d372be7ad
John Bresnahan <john@stardog.com>
2017-04-22 11:06:10 -1000

    Fixing it

--- ci/build-graviton.sh
+++ ci/build-graviton.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 GRAV_REPO=$3
 OUT_DIR=$4
+STAGE_BUCKET=$5
 THIS_DIR=$(pwd)
 
 # we know what the docker container looks like
@@ -20,10 +21,12 @@ VER=$(cat etc/version | sed 's/-.*//')
 gox -osarch="linux/amd64" -osarch="darwin/amd64" -output=$THIS_DIR/$OUT_DIR/$VER/{{.OS}}/stardog-graviton
 
 DATE=$(date +%F-%H-%M-%S)
-LINUX_BUCKET_NAME=$VER/$DATE/linux/stardog-graviton
-OSX_BUCKET_NAME=$VER/$DATE/darwin/stardog-graviton
+LINUX_BUCKET_NAME=s3://$STAGE_BUCKET/$VER/$DATE/linux/stardog-graviton
+OSX_BUCKET_NAME=s3://$STAGE_BUCKET/$VER/$DATE/darwin/stardog-graviton
 
-aws cli cp $THIS_DIR/$OUT_DIR/$VER/linux/stardog-graviton $LINUX_BUCKET_NAME
-aws cli cp $THIS_DIR/$OUT_DIR/$VER/darwin/stardog-graviton $DARWIN_BUCKET_NAME
+echo "XXX aws s3 cp $THIS_DIR/$OUT_DIR/$VER/linux/stardog-graviton $LINUX_BUCKET_NAME"
+aws s3 cp $THIS_DIR/$OUT_DIR/$VER/linux/stardog-graviton $LINUX_BUCKET_NAME
+echo "YYY aws s3 cp $THIS_DIR/$OUT_DIR/$VER/darwin/stardog-graviton $OSX_BUCKET_NAME"
+aws s3 cp $THIS_DIR/$OUT_DIR/$VER/darwin/stardog-graviton $OSX_BUCKET_NAME
 
-echo $LINUX_BUCKET_NAME > $OUT_DIR/s3filename
+echo $LINUX_BUCKET_NAME > $THIS_DIR/$OUT_DIR/s3filename
